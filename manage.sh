5ce6ab6d2faffad5e0780b3d040a5197cc914f6a
thomas-beguin <thomas.beguin@polynhome.io>
2023-10-04 14:14:26 +0200

    Dockerfile dev for local and Dockerfile prod for controller

--- manage.sh
+++ manage.sh
@@ -2,10 +2,16 @@ CONTAINER_NAME="ffmpeg"
 
 function main() {
     case "$1" in
-        "run")
+        "dev")
             docker stop $CONTAINER_NAME
             docker rm $CONTAINER_NAME
-            docker build -t ffmpeg .
+            docker build -t ffmpeg -f etc/Dockerfile-dev .
+            docker run -it --network=host --name=$CONTAINER_NAME ffmpeg
+            ;;
+        "prod")
+            docker stop $CONTAINER_NAME
+            docker rm $CONTAINER_NAME
+            docker build -t ffmpeg -f etc/Dockerfile-prod .
             docker run -it --network=host --name=$CONTAINER_NAME ffmpeg
             ;;
         *)
